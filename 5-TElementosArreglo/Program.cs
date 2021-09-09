0a25df1c14df34ff128832e5ad205c06495e547a
Melicha <melissa.chavez@tectijuana.edu.mx>
2021-09-08 22:46:04 -0700

    Imprimir

---
+++ 5-TElementosArreglo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using C = System.Console;
+
+namespace _5_TElementosArreglo
+{
+    /// Practica 5
+    /// 5.- Desplegar todos los elementos de un arreglo.
+
+    /// Chávez Pérez Melissa
+    /// #M21210035
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C.BackgroundColor = ConsoleColor.Blue;
+            C.ForegroundColor = ConsoleColor.White;
+
+            //Declaramos arreglo
+            int[, ] numeros = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            string[] lenguajes = { "C#", "C", "C++",  "Java", "PHP", "JavaScript", "Python" };
+
+            //Desplegamos variables en string
+            C.WriteLine("\n------------------------------------\nImprimiendo arreglo de Lenguajes \n------------------------------------\n");
+
+            foreach (string lenguaje in lenguajes)
+            {
+                C.WriteLine(lenguaje);
+            }
+
+            //Desplegamos variebles de int
+            C.WriteLine("\n------------------------------------\nImprimiendo arreglo de Numeros \n------------------------------------\n");
+            foreach (var elemento in numeros)
+            {
+                C.WriteLine(elemento);
+            }
+
+
+            C.ReadLine();
+        }
+         
+    }
+}
